163c8815d884ca0a80b690a1881bc368659e7fd8
Pawan Poudel <pawan.poudel@gmail.com>
2019-07-19 21:22:54 -0400

    3.11 - Let Expression

---
+++ chapter-3/3.11-let-expression/beginning-elm/src/Playground.elm
@@ -0,0 +1,53 @@
+module Playground exposing (main)
+
+import Html
+
+
+escapeEarth myVelocity mySpeed fuelStatus =
+    let
+        escapeVelocityInKmPerSec =
+            11.186
+
+        orbitalSpeedInKmPerSec =
+            7.67
+
+        whereToLand =
+            if fuelStatus == "low" then
+                "Land on droneship"
+
+            else
+                "Land on launchpad"
+    in
+    if myVelocity > escapeVelocityInKmPerSec then
+        "Godspeed"
+
+    else if mySpeed == orbitalSpeedInKmPerSec then
+        "Stay in orbit"
+
+    else
+        whereToLand
+
+
+computeSpeed distance time =
+    distance / time
+
+
+computeTime startTime endTime =
+    endTime - startTime
+
+
+add a b =
+    a + b
+
+
+multiply c d =
+    c * d
+
+
+divide e f =
+    e / f
+
+
+main =
+    escapeEarth 10 6.7 "low"
+        |> Html.text
