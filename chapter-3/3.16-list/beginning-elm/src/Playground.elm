0c1733a25ccddac7670622299023bba7103e6c56
Pawan Poudel <pawan.poudel@gmail.com>
2019-07-22 01:46:18 -0400

    3.16 - List

---
+++ chapter-3/3.16-list/beginning-elm/src/Playground.elm
@@ -0,0 +1,156 @@
+module Playground exposing (main)
+
+import Html
+
+
+escapeEarth myVelocity mySpeed fuelStatus =
+    let
+        escapeVelocityInKmPerSec =
+            11.186
+
+        orbitalSpeedInKmPerSec =
+            7.67
+
+        whereToLand =
+            if fuelStatus == "low" then
+                "Land on droneship"
+
+            else
+                "Land on launchpad"
+    in
+    if myVelocity > escapeVelocityInKmPerSec then
+        "Godspeed"
+
+    else if mySpeed == orbitalSpeedInKmPerSec then
+        "Stay in orbit"
+
+    else
+        whereToLand
+
+
+computeSpeed distance time =
+    distance / time
+
+
+computeTime startTime endTime =
+    endTime - startTime
+
+
+add a b =
+    a + b
+
+
+multiply c d =
+    c * d
+
+
+divide e f =
+    e / f
+
+
+weekday dayInNumber =
+    case dayInNumber of
+        0 ->
+            "Sunday"
+
+        1 ->
+            "Monday"
+
+        2 ->
+            "Tuesday"
+
+        3 ->
+            "Wednesday"
+
+        4 ->
+            "Thursday"
+
+        5 ->
+            "Friday"
+
+        6 ->
+            "Saturday"
+
+        _ ->
+            "Unknown day"
+
+
+hashtag dayInNumber =
+    case weekday dayInNumber of
+        "Sunday" ->
+            "#SinDay"
+
+        "Monday" ->
+            "#MondayBlues"
+
+        "Tuesday" ->
+            "#TakeMeBackTuesday"
+
+        "Wednesday" ->
+            "#HumpDay"
+
+        "Thursday" ->
+            "#ThrowbackThursday"
+
+        "Friday" ->
+            "#FlashbackFriday"
+
+        "Saturday" ->
+            "#Caturday"
+
+        _ ->
+            "#Whatever"
+
+
+revelation =
+    """
+    It became very clear to me sitting out there today
+    that every decision I've made in my entire life has
+    been wrong. My life is the complete "opposite" of
+    everything I want it to be. Every instinct I have,
+    in every aspect of life, be it something to wear,
+    something to eat - it's all been wrong.
+    """
+
+
+descending a b =
+    case compare a b of
+        LT ->
+            GT
+
+        GT ->
+            LT
+
+        EQ ->
+            EQ
+
+
+evilometer character1 character2 =
+    case ( character1, character2 ) of
+        ( "Joffrey", "Ramsay" ) ->
+            LT
+
+        ( "Joffrey", "Night King" ) ->
+            LT
+
+        ( "Ramsay", "Joffrey" ) ->
+            GT
+
+        ( "Ramsay", "Night King" ) ->
+            LT
+
+        ( "Night King", "Joffrey" ) ->
+            GT
+
+        ( "Night King", "Ramsay" ) ->
+            GT
+
+        _ ->
+            GT
+
+
+main =
+    [ "Night King", "Joffrey", "Ramsay" ]
+        |> List.sortWith evilometer
+        |> Debug.toString
+        |> Html.text
