f57e344aef105717fa684328a2c7c71020340f92
Pawan Poudel <pawan.poudel@gmail.com>
2019-07-27 14:15:40 -0400

    4.2 - Immutability

---
+++ chapter-4/4.2-immutability/beginning-elm/experiment.js
@@ -0,0 +1,21 @@
+var scoreMultiplier = 2;
+var highestScores = [316, 320, 312, 370, 337, 318, 314];
+
+function doubleScores(scores) {
+    var newScores = [];
+
+    for (var i = 0; i < scores.length; i++) {
+        newScores[i] = scores[i] * scoreMultiplier;
+    }
+
+    return newScores;
+}
+
+
+function scoresLessThan320(scores) {
+    return scores.filter(isLessThan320);
+}
+
+function isLessThan320(score) {
+    return score < 320;
+}
