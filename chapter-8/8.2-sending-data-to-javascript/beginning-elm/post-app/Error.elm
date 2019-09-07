ed7185551a28ac9a5e2852cde7a1adc0ab2e84dc
Pawan Poudel <pawan.poudel@gmail.com>
2019-09-07 16:06:27 -0400

    8.2 - Sending Data to JavaScript

---
+++ chapter-8/8.2-sending-data-to-javascript/beginning-elm/post-app/Error.elm
@@ -0,0 +1,22 @@
+module Error exposing (buildErrorMessage)
+
+import Http
+
+
+buildErrorMessage : Http.Error -> String
+buildErrorMessage httpError =
+    case httpError of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Server is taking too long to respond. Please try again later."
+
+        Http.NetworkError ->
+            "Unable to reach server."
+
+        Http.BadStatus statusCode ->
+            "Request failed with status code: " ++ String.fromInt statusCode
+
+        Http.BadBody message ->
+            message
